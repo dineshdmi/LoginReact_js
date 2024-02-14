5f3ae3444908e4f3d5cbe94fdc3029c698796b04
dineshdmi <mdinu8127@gmail.com>
2024-02-14 17:07:35 +0530

    add env

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import SIgn_img from './SIgn_img'
+import Sign_img from './SIgn_img'
 import { NavLink } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
@@ -110,7 +110,7 @@ const Home = () => {
                         </Form>
                         <p className='mt-3'>Already Have an Account <span><NavLink to="/login">SignIn</NavLink></span> </p>
                     </div>
-                    <SIgn_img/>
+                    <Sign_img/>
                 </section>
                 <ToastContainer />
             </div>
